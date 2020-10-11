b59e7fa5aa03bea9a7587d79f008781dbf3da6f2
meganr28 <meganr28@gmail.com>
2020-10-11 07:46:14 -0400

    add rest of images

--- DigitalCatalog/imagemanager.js
+++ DigitalCatalog/imagemanager.js
@@ -2,6 +2,7 @@ class ImageManager {
   
   constructor() {
     this.story = [];
+    this.mb = [];
     this.characters = [];
     this.environment = [];
     this.props = [];
@@ -9,16 +10,42 @@ class ImageManager {
   
   loadStory() {
     let tree;
+    let witch_rough, witch_final, fairy_rough, fairy_final, alm_rough, alm_final;
     tree = loadImage('images/environment/tree_transparent.png');
-    this.story = [tree];
+    witch_rough = loadImage('images/story/witch_rough.jpg');
+    witch_final = loadImage('images/story/witch_final.png');
+    fairy_rough = loadImage('images/story/fairy_rough.png');
+    fairy_final = loadImage('images/story/fairy_final.png');
+    alm_rough = loadImage('images/story/alm_rough.png');
+    alm_final = loadImage('images/story/alm_final.png');
+    this.story = [tree, witch_rough, witch_final, fairy_rough, fairy_final, alm_rough, alm_final];
+  }
+  
+  loadMB() {
+    let mb1, mb2, mb3, mb4, mb5, mb6;
+    mb1 = loadImage('images/moodBoard/mb1.jpg');
+    mb2 = loadImage('images/moodBoard/mb2.jpg');
+    mb3 = loadImage('images/moodBoard/mb3.jpg');
+    mb4 = loadImage('images/moodBoard/mb4.jpg');
+    mb5 = loadImage('images/moodBoard/mb5.jpg');
+    mb6 = loadImage('images/moodBoard/mb6.jpg');
+    this.mb = [mb1, mb2, mb3, mb4, mb5, mb6];
   }
   
   loadChar() {
     let w_expr, w_poses, w_turn;
+    let f_expr, f_poses, f_turn;
+    let a_expr, a_poses, a_turn;
     w_expr = loadImage('images/character/witch_expressions.png');
     w_poses = loadImage('images/character/witch_poses_3.png');
     w_turn = loadImage('images/character/witch_turnaround.png');
-    this.characters = [w_expr, w_poses, w_turn];
+    f_expr = loadImage('images/character/fairy_expressions.jpg');
+    f_poses = loadImage('images/character/fairy_poses_2.jpg');
+    f_turn = loadImage('images/character/fairy_turnaround.jpg');
+    a_expr = loadImage('images/character/alm_expressions.png');
+    a_poses = loadImage('images/character/alm_poses_3.png');
+    a_turn = loadImage('images/character/alm_turnaround.png');
+    this.characters = [w_expr, w_poses, w_turn, f_expr, f_poses, f_turn, a_expr, a_poses, a_turn];
   }
   
   loadEnv() {

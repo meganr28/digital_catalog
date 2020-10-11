cdd7e26ee92aae330112e8e38265fc31b6bf10b3
meganr28 <meganr28@gmail.com>
2020-10-10 23:26:26 -0400

    load environment images

--- DigitalCatalog/imagemanager.js
+++ DigitalCatalog/imagemanager.js
@@ -1,1 +1,20 @@
+class ImageManager {
+  
+  constructor() {
+    this.environment = [];
+    this.props = [];
+  }
 
+  loadEnv() {
+    let bg_rough, bg_color_1, bg_color_2;
+    bg_rough = loadImage('images/environment/bg_rough.png');
+    bg_color_1 = loadImage('images/environment/bg_color_1.png');
+    bg_color_2 = loadImage('images/environment/bg_color_2.png');
+    this.environment = [bg_rough, bg_color_1, bg_color_2];
+  }
+  
+  loadProps() {
+    let potions_box, spellbook;
+  }
+  
+}
